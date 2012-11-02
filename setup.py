6585ccb9a7d4848f81c56babc05988d5a90c969d
Jonas Wagner <jwagner@raptus.com>
2012-11-02 12:59:15 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='raptus.multilanguageplone',
       version=version,
       description="adding multilanguage support to default plone content types",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -17,7 +17,7 @@ setup(name='raptus.multilanguageplone',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.multilanguageplone',
+      url='https://github.com/Raptus/raptus.multilanguageplone',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus'],
